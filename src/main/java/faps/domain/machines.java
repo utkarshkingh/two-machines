88ccdf3c5d1573e018df3489c87aa35b2d96f9d7
utkarshkingh <ajjumazsan@gmail.com>
2023-12-22 11:40:41 +0100

    Added my project

---
+++ src/main/java/faps/domain/machines.java
@@ -0,0 +1,20 @@
+package faps.domain;
+
+import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.lookup.PlanningId;
+import org.optaplanner.core.api.domain.variable.PlanningVariable;
+
+
+
+public class machines {
+
+    @PlanningId
+
+     int machine1car=10;
+     int machine2car=12;
+     int machine1truck=20;
+     int machine2truck=24;
+     float machine1change=0.5f;
+     float machine2change=0.5f;
+
+}
