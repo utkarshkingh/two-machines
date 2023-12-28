0db72bf4e0876e3b4b143fc1bc538ca3974d6def
utkarshkingh <ajjumazsan@gmail.com>
2023-12-28 14:32:24 +0100

    removed job length restriction

--- src/main/java/faps/domain/machines.java
+++ src/main/java/faps/domain/machines.java
@@ -7,17 +7,17 @@ import org.optaplanner.core.api.domain.variable.PlanningVariable;
 
 public class machines {
 
-     @PlanningId
+     
      int machine1car=10;
-     @PlanningId
+     
      int machine2car=12;
-     @PlanningId
+     
      int machine1truck=20;
-     @PlanningId
+     
      int machine2truck=24;
-     @PlanningId
+     
      float machine1change=0.5f;
-     @PlanningId
+     
      float machine2change=0.5f;
 
 }
--- src/main/java/faps/domain/sequence.java
+++ src/main/java/faps/domain/sequence.java
@@ -2,63 +2,61 @@ package faps.domain;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Arrays;
 
 import org.optaplanner.core.api.domain.entity.PlanningEntity;
 import org.optaplanner.core.api.domain.lookup.PlanningId;
 import org.optaplanner.core.api.domain.variable.PlanningVariable;
 
 @PlanningEntity
+
+
 public class sequence {
 
-    
+    @PlanningVariable()
+    public sequence sequence1;
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-    
-            // Read the number of inputs
-            System.out.print("Enter the number of jobs: ");
-            int jobs = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
-    
-            // Create an array to store 'c' and 't'
-            char[] sequence = new char[jobs];
-    
-            // Read input string
-            System.out.print("Enter a string of 'c' and 't': ");
-            String inputString = scanner.nextLine();
-    
-            // Check if the length of the input string matches the number of jobs
-            if (inputString.length() != jobs) {
-                System.out.println("Invalid input length. Please enter exactly " + jobs + " characters.");
+    @PlanningVariable()
+    public sequence sequence2;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read input string
+        System.out.print("Enter a string of 'c' and 't': ");
+        String inputString = scanner.nextLine();
+
+        // Create an array to store 'c' and 't'
+        char[] sequence = new char[inputString.length()];
+
+        // Fill the array with characters from the input string
+        for (int i = 0; i < inputString.length(); i++) {
+            char c = inputString.charAt(i);
+            // Check if the character is 'c' or 't' and add it to the array
+            if (c == 'c' || c == 't') {
+                sequence[i] = c;
+            } else {
+                System.out.println("Invalid character detected. Only 'c' and 't' are allowed.");
                 return;
             }
-    
-            // Fill the array with characters from the input string
-            for (int i = 0; i < jobs; i++) {
-                char c = inputString.charAt(i);
-                // Check if the character is 'c' or 't' and add it to the array
-                if (c == 'c' || c == 't') {
-                    sequence[i] = c;
-                } else {
-                    System.out.println("Invalid character detected. Only 'c' and 't' are allowed.");
-                    
-                    return;
-                }
-            }
-    
-            // Display the array
-            System.out.print("Array of 'c' and 't': ");
-            for (char c : sequence) {
-                System.out.print(c + " ");
-            }
         }
+
+        // Display the array
+        System.out.print("Array of 'c' and 't': ");
+        for (char c : sequence) {
+            System.out.print(c + " ");
+        }
+
+        
+
     }
+}
 
     
     
 
 
 
-    
 
+    
 
