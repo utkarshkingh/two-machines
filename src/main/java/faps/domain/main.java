e5f1ceea2d717d0fc0303e4de9c24fa1e8025be3
utkarshkingh <ajjumazsan@gmail.com>
2024-01-02 16:35:28 +0100

    a

--- src/main/java/faps/domain/main.java
+++ src/main/java/faps/domain/main.java
@@ -1,10 +1,27 @@
 package faps.domain;
 
+import org.optaplanner.core.api.solver.SolverFactory;
+
 public class main {
 
     public static void main(String[] args) {
+
+        SolverFactory<TabuSe> solverFactory = SolverFactory.createFromXmlResource(
+                "org/optaplanner/examples/cloudbalancing/solver/cloudBalancingSolverConfig.xml");
+        Solver<CloudBalance> solver = solverFactory.buildSolver();
+
+        // Load a problem with 400 computers and 1200 processes
+        CloudBalance unsolvedCloudBalance = new CloudBalancingGenerator().createCloudBalance(400, 1200);
+
+        // Solve the problem
+        CloudBalance solvedCloudBalance = solver.solve(unsolvedCloudBalance);
+
+        // Display the result
+        System.out.println("\nSolved cloudBalance with 400 computers and 1200 processes:\n"
+                + toDisplayString(solvedCloudBalance));
+    }
         
     }
 
-}
+
 
