4c24af0b58a7bcc577c9d1731aed6dd0e8a474e6
utkarshkingh <ajjumazsan@gmail.com>
2024-01-02 16:00:04 +0100

    commit

---
+++ src/main/java/faps/domain/runner.java
@@ -0,0 +1,19 @@
+package faps.domain;
+
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+
+@PlanningSolution
+
+public class runner {
+
+private List<sequence> getsequence
+
+private HardSoftScore score;
+
+
+@PlanningEntityCollectionProperty
+ public 
+
+}
