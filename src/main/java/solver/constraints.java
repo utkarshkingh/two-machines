d869c79d0187b34ad2a61a88cf0c6e222a06d1cc
utkarshkingh <ajjumazsan@gmail.com>
2024-01-09 10:58:10 +0100

    updates

--- src/main/java/solver/constraints.java
+++ src/main/java/solver/constraints.java
@@ -1,5 +1,48 @@
 package solver;
 
-public class constraints {
+import java.util.function.Function;
 
+import org.optaplanner.core.api.score.buildin.bendable.BendableScore;
+import org.optaplanner.core.api.score.stream.Constraint;
+import org.optaplanner.core.api.score.stream.ConstraintFactory;
+import org.optaplanner.core.api.score.stream.ConstraintProvider;
+import org.optaplanner.core.api.score.stream.uni.UniConstraintStream;
+
+
+import faps.domain.machines;
+
+import org.optaplanner.core.api.score.stream.Joiners;
+
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+
+
+
+public class constraints implements ConstraintProvider {
+
+    @Override
+    public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
+        return new Constraint[]{
+                minimizeMakespan(constraintFactory),
+            
+        };
+    }
+
+    private Constraint minimizeMakespan(ConstraintFactory constraintFactory) {
+        return constraintFactory.forEach(machines.class)
+                .join(machines.class, Joiners.equal((machine1, machine2) -> machine1, Function.identity()))
+                .groupBy((machine1, machine2) -> machine1)
+                .penalize("Minimize makespan", HardSoftScore.ONE_SOFT,
+                        (machines1, machines2) -> calculateMakespanPenalty(machines1, machines2));
+    }
+
+    
+
+    
+    private int calculateMakespanPenalty(machines machines1, machines machines2) {
+        
+        return 0;
+    }
+
+   
 }
+
